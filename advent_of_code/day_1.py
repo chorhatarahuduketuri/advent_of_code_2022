5bffc0b28b204670e522acb68827a32fcce96d9d
chhk <chorhatarahuduketuri@gmail.com>
2022-12-01 09:04:24 +0000

    Testing setup, and day 1 tests

--- advent_of_code/day_1.py
+++ advent_of_code/day_1.py
@@ -1,2 +1,26 @@
-print('day  1, star  1: ', max([sum([int(calorie) for calorie in line.split()]) for line in open('../puzzle_inputs/day1.txt').read().split('\n\n')]))
-print('day  1, star  2: ', sum(sorted([sum([int(calorie) for calorie in line.split()]) for line in open('../puzzle_inputs/day1.txt').read().split('\n\n')], reverse=True)[:3]))
+from settings import PUZZLE_INPUT_PATH
+
+
+def compute_star_1(puzzle_input):
+    return max(
+        [
+            sum([int(calorie) for calorie in line.split()])
+            for line in puzzle_input.split("\n\n")
+        ]
+    )
+
+
+def compute_star_2(puzzle_input):
+    return sum(
+        sorted(
+            [
+                sum([int(calorie) for calorie in line.split()])
+                for line in puzzle_input.split("\n\n")
+            ],
+            reverse=True,
+        )[:3]
+    )
+
+
+print("day  1, star  1: ", compute_star_1(open(PUZZLE_INPUT_PATH / "day1.txt").read()))
+print("day  1, star  2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day1.txt").read()))

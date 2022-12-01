20f9405b8bd823128ab3f65b7065032327e1b1b4
chhk <chorhatarahuduketuri@gmail.com>
2022-12-01 07:59:44 +0000

    Day 1 Star 2

--- advent_of_code/day_1.py
+++ advent_of_code/day_1.py
@@ -1,1 +1,2 @@
 print('day  1, star  1: ', max([sum([int(calorie) for calorie in line.split()]) for line in open('../puzzle_inputs/day1.txt').read().split('\n\n')]))
+print('day  1, star  2: ', sum(sorted([sum([int(calorie) for calorie in line.split()]) for line in open('../puzzle_inputs/day1.txt').read().split('\n\n')], reverse=True)[:3]))

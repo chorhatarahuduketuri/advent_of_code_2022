b52df2f6baab9f4c3c50690cd27e8bd392744239
chhk <chorhatarahuduketuri@gmail.com>
2022-12-04 08:40:46 +0000

    Day 4 Star 1

--- advent_of_code/day_4.py
+++ advent_of_code/day_4.py
@@ -2,7 +2,27 @@ from settings import PUZZLE_INPUT_PATH
 
 
 def compute_star_1(puzzle_input):
-    return
+    return sum(
+        [
+            (
+                pair_of_elves[0][0] <= pair_of_elves[1][0]
+                and pair_of_elves[0][1] >= pair_of_elves[1][1]
+            )
+            or (
+                pair_of_elves[1][0] <= pair_of_elves[0][0]
+                and pair_of_elves[1][1] >= pair_of_elves[0][1]
+            )
+            for pair_of_elves in [
+                [
+                    [int(position) for position in assignment.split("-")]
+                    for assignment in pair
+                ]
+                for pair in [
+                    line.split(",") for line in puzzle_input.strip().split("\n")
+                ]
+            ]
+        ]
+    )
 
 
 def compute_star_2(puzzle_input):

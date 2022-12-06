0c8fd3763e13ef61fdc41ef12ec10117658d32aa
chhk <chorhatarahuduketuri@gmail.com>
2022-12-06 08:42:06 +0000

    Day 5 Star 2

--- advent_of_code/day_5.py
+++ advent_of_code/day_5.py
@@ -41,9 +41,25 @@ def compute_star_1(puzzle_input):
     return "".join([stack[-1] for stack in stacks])
 
 
-# def compute_star_2(puzzle_input):
-#     return
+def execute_9001_moves(stacks: list[list[str]], moves: str) -> list[list[str]]:
+    [
+        [
+            stacks[operation[2] - 1].extend(stacks[operation[1] - 1][-operation[0] :]),
+            [stacks[operation[1] - 1].pop() for count in range(operation[0])],
+        ]
+        for operation in [
+            [int(number) for number in move.split()[1::2]] for move in moves.split("\n")
+        ]
+    ]
+    return stacks
+
+
+def compute_star_2(puzzle_input):
+    initial_position, moves = puzzle_input.rstrip().split("\n\n")
+    stacks = setup_stacks_from_initial_position(initial_position)
+    stacks = execute_9001_moves(stacks, moves)
+    return "".join([stack[-1] for stack in stacks])
 
 
 print("day  5, star  1: ", compute_star_1(open(PUZZLE_INPUT_PATH / "day5.txt").read()))
-# print("day  5, star  2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day5.txt").read()))
+print("day  5, star  2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day5.txt").read()))
--- tests/test_day_5.py
+++ tests/test_day_5.py
@@ -12,10 +12,10 @@ move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2"""
 output_day_5_star_1 = "CMZ"
-# output_day_5_star_2 =
+output_day_5_star_2 = "MCD"
 
 day_5_star_1_test_input = [(input_day_5, output_day_5_star_1)]
-# day_5_star_2_test_input = [(input_day_5, output_day_5_star_2)]
+day_5_star_2_test_input = [(input_day_5, output_day_5_star_2)]
 
 
 @pytest.mark.parametrize("puzzle_input, output", day_5_star_1_test_input)
@@ -23,6 +23,6 @@ def test_day_5_star_1(puzzle_input, output):
     assert day_5.compute_star_1(puzzle_input) == output
 
 
-# @pytest.mark.parametrize("puzzle_input, output", day_5_star_2_test_input)
-# def test_day_5_star_2(puzzle_input, output):
-#     assert day_5.compute_star_2(puzzle_input) == output
+@pytest.mark.parametrize("puzzle_input, output", day_5_star_2_test_input)
+def test_day_5_star_2(puzzle_input, output):
+    assert day_5.compute_star_2(puzzle_input) == output

e4613911eed001cce7a658e5393691f22671759e
chhk <chorhatarahuduketuri@gmail.com>
2022-12-09 08:49:12 +0000

    Day 7 Star 1

---
+++ advent_of_code/day_7.py
@@ -0,0 +1,86 @@
+from settings import PUZZLE_INPUT_PATH
+
+
+class Node:
+    def __init__(self, name, parent):
+        self.name = name
+        self.parent = parent
+        self.children = {}
+        self.files = []
+        self.size = 0
+
+    def add_child(self, child_name):
+        self.children[child_name] = Node(child_name, self)
+
+    def add_file(self, filename, size):
+        self.files.append((filename, size))
+
+    def get_file_sizes(self) -> int:
+        return sum(int(size) for (file, size) in self.files)
+
+    def get_child_sizes(self) -> int:
+        return sum(child.size for child in self.children.values())
+
+    def update_size(self):
+        self.size = self.get_file_sizes() + self.get_child_sizes()
+
+
+def grow_tree(node: Node, rep_loops: list):
+    if not rep_loops:
+        return node
+    cmd = rep_loops[0]
+    remaining_rep_loops = rep_loops[1:]
+    if cmd[0][0] == "cd":
+        if cmd[0][1] == "..":
+            return grow_tree(node=node.parent, rep_loops=remaining_rep_loops)
+        else:
+            if cmd[0][1] in node.children.keys():
+                return grow_tree(
+                    node=node.children[cmd[0][1]], rep_loops=remaining_rep_loops
+                )
+            else:
+                return grow_tree(
+                    node=Node(cmd[0][1], parent=node), rep_loops=remaining_rep_loops
+                )
+    else:  # cmd[0][0] == 'ls'
+        for output in cmd[1:]:
+            if output[0] == "dir":
+                if output[1] not in node.children.keys():
+                    node.add_child(output[1])
+            else:  # output[0] == a number
+                node.add_file(output[1], output[0])
+        return grow_tree(node=node, rep_loops=remaining_rep_loops)
+
+
+def map_dir_sizes(node, dir_sizes: list[tuple[str, int]]):
+    if not node.children:
+        node.update_size()
+        dir_sizes.append((node.name, node.size))
+        return dir_sizes
+    for child in node.children.values():
+        map_dir_sizes(node=child, dir_sizes=dir_sizes)
+    node.update_size()
+    dir_sizes.append((node.name, node.size))
+    return dir_sizes
+
+
+def compute_star_1(puzzle_input):
+    rep_loops = [
+        [split.split() for split in split_cmds.strip().split("\n")]
+        for split_cmds in puzzle_input.strip().split("$")
+        if split_cmds
+    ]
+    rep_loops.pop(0)  # Drop the root node's creation
+    root_node = Node("root", None)
+    grow_tree(root_node, rep_loops)
+    dir_sizes = map_dir_sizes(root_node, [])
+    return sum(size for (name, size) in dir_sizes if size <= 100000)
+
+
+# def compute_star_2(puzzle_input):
+#     return
+
+
+print(
+    "day  7, star  1: ", compute_star_1(open(PUZZLE_INPUT_PATH / "day7.txt").read())
+)  # print("day  7, star  2: ", compute_star_2(open(PUZZLE_INPUT_PATH / "day7.txt").read()))
---
+++ tests/test_day_7.py
@@ -0,0 +1,42 @@
+import pytest
+
+from advent_of_code import day_7
+
+input_day_7 = """$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k"""
+output_day_7_star_1 = 95437
+# output_day_7_star_2 =
+
+day_7_star_1_test_input = [(input_day_7, output_day_7_star_1)]
+# day_7_star_2_test_input = [(input_day_7, output_day_7_star_2)]
+
+
+@pytest.mark.parametrize("puzzle_input, output", day_7_star_1_test_input)
+def test_day_7_star_1(puzzle_input, output):
+    assert day_7.compute_star_1(puzzle_input) == output
+
+
+# @pytest.mark.parametrize("puzzle_input, output", day_7_star_2_test_input)
+# def test_day_7_star_2(puzzle_input, output):
+#     assert day_7.compute_star_2(puzzle_input) == output
